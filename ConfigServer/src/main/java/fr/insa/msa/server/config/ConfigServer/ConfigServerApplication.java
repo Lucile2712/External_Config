c92660e5f7821d3cf196a3bda1f5bf7b633a28c5
SaladeVerteee <cossoul.lucile@gmail.com>
2024-10-18 17:22:32 +0200

    Config files added

--- ConfigServer/src/main/java/fr/insa/msa/server/config/ConfigServer/ConfigServerApplication.java
+++ ConfigServer/src/main/java/fr/insa/msa/server/config/ConfigServer/ConfigServerApplication.java
@@ -1,12 +1,15 @@
 package fr.insa.msa.server.config.ConfigServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
